cb6b00a938d552ebdd9afefe8ebd05fe92a4b56c
sameepv21 <sameep0112vani@gmail.com>
2024-08-19 16:36:19 -0700

    [feat]: refactor all scripts and completed
    [test]: all testing left

---
+++ scripts/finetune_llava_qa.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+deepspeed --master_port=25640 --include=localhost:0 --module llava.train.train_mem \
+    --deepspeed /home/svani/LVLM/temp/LLaVA/scripts/zero2.json \
+    --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
+    --model_name_or_path liuhaotian/llava-v1.6-vicuna-13b \
+    --version plain \
+    --data_path /scratch/svani/data/playground/data/llava_instruct_80k.json \
+    --image_folder /scratch/svani/data/playground/data/coco/train2017 \
+    --vision_tower openai/clip-vit-large-patch14-336 \
+    --mm_projector_type mlp2x_gelu \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --bf16 True \
+    --output_dir /scratch/svani/experiments/dci_experiments/llava-v1.6-vicuna-13b-lora \
+    --num_train_epochs 6 \
+    --per_device_train_batch_size 32 \
+    --per_device_eval_batch_size 32 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 100 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 100 \
+    --tf32 True \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
+    --lazy_preprocess True \
+    --report_to wandb
